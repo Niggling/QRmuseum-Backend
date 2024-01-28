96f086e6869bb052f8b318dbcad0a1b912c2daa1
Niggling <272744476@qq.com>
2024-01-28 21:01:11 +0800

    新增：跨域配置

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,21 @@
 package com.example.demo;
 
+import com.example.demo.config.CorsConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @MapperScan("com.example.demo.mapper")
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        return new CorsConfig().corsFilter();
     }
 }
